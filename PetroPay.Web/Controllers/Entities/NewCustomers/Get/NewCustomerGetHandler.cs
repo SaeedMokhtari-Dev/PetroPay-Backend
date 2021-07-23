4051b8300e7e014ad45e0bcc54d219b3cbbe1038
Saeed <saeedmokhtari.dev@gmail.com>
2021-07-23 11:53:11 +0430

    new customer page -Add Date From and To And Status Filter -Excel Export

--- PetroPay.Web/Controllers/Entities/NewCustomers/Get/NewCustomerGetHandler.cs
+++ PetroPay.Web/Controllers/Entities/NewCustomers/Get/NewCustomerGetHandler.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using PetroPay.Core.Api.Handlers;
 using PetroPay.Core.Api.Models;
+using PetroPay.Core.Constants;
 using PetroPay.DataAccess.Contexts;
+using PetroPay.DataAccess.Entities;
 
 namespace PetroPay.Web.Controllers.Entities.NewCustomers.Get
 {
@@ -24,17 +28,51 @@ namespace PetroPay.Web.Controllers.Entities.NewCustomers.Get
         protected override async Task<ActionResult> Execute(NewCustomerGetRequest request)
         {
             var query = _context.NewCustomers.OrderByDescending(w => w.CustReqId)
-                .Skip(request.PageIndex * request.PageSize).Take(request.PageSize)
                 .AsQueryable();
+
+            query = createQuery(query, request);
+            
+            NewCustomerGetResponse response = new NewCustomerGetResponse();
+            response.TotalCount = await query.CountAsync();
+            
+            if(!request.ExportToFile)
+                query = query.Skip(request.PageIndex * request.PageSize).Take(request.PageSize);
 
             var result = await query.ToListAsync();
 
             var mappedResult = _mapper.Map<List<NewCustomerGetResponseItem>>(result);
 
-            NewCustomerGetResponse response = new NewCustomerGetResponse();
-            response.TotalCount = await _context.NewCustomers.CountAsync();
             response.Items = mappedResult;
             return ActionResult.Ok(response);
+        }
+        private IQueryable<NewCustomer> createQuery(IQueryable<NewCustomer> query, NewCustomerGetRequest request)
+        {
+            if (!string.IsNullOrEmpty(request.DateFrom))
+            {
+                DateTime dateTimeFrom = DateTime.ParseExact(request.DateFrom, DateTimeConstants.DateFormat,
+                    CultureInfo.InvariantCulture);
+                query = query.Where(w => w.CutReqDatetime.HasValue && w.CutReqDatetime.Value >= dateTimeFrom);
+            }
+            if (!string.IsNullOrEmpty(request.DateTo))
+            {
+                DateTime dateTimeTo = DateTime.ParseExact(request.DateTo, DateTimeConstants.DateFormat,
+                    CultureInfo.InvariantCulture);
+                query = query.Where(w => w.CutReqDatetime.HasValue && w.CutReqDatetime.Value <= dateTimeTo);
+            }
+            if (request.Status.HasValue)
+            {
+                switch (request.Status.Value)
+                {
+                    case 1: //Confirmed
+                        query = query.Where(w => w.CustReqStatus.HasValue && w.CustReqStatus == true);
+                        break;
+                    case 2: //NotConfirmed
+                        query = query.Where(w => w.CustReqStatus == null || w.CustReqStatus == false);
+                        break;
+                }
+            }
+            return query;
+
         }
     }
 }
--- PetroPay.Web/Controllers/Entities/NewCustomers/Get/NewCustomerGetRequest.cs
+++ PetroPay.Web/Controllers/Entities/NewCustomers/Get/NewCustomerGetRequest.cs
@@ -2,6 +2,10 @@ namespace PetroPay.Web.Controllers.Entities.NewCustomers.Get
 {
     public class NewCustomerGetRequest
     {
+        public string DateFrom { get; set; }
+        public string DateTo { get; set; }
+        public int? Status { get; set; }
+        public bool ExportToFile { get; set; }
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
     }
