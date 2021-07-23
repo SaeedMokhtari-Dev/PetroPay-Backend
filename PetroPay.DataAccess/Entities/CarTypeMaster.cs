557bb5f8767587972c6d1c0fb44cb3500aead478
Saeed <saeedmokhtari.dev@gmail.com>
2021-07-23 13:53:33 +0430

    -Read Data From "Car_type_master" Table, and set in carType Field in Frontend. -add "station_bonus_balance" field in admin dashboard.

---
+++ PetroPay.DataAccess/Entities/CarTypeMaster.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+
+namespace PetroPay.DataAccess.Entities
+{
+    public partial class CarTypeMaster
+    {
+        public int CarTypeId { get; set; }
+        public string CarTypeName { get; set; }
+        public int? CarBonusPoint { get; set; }
+    }
+}
